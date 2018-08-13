9bc4eb612e4973e724779829fc29825464dc308a
Yuyao Xue <xueyuyao@ftang.cn>
2018-08-13 13:08:38 +0800

    提交第二，三章的homework

---
+++ lua/lua_unit3.lua
@@ -0,0 +1,21 @@
+--[[ 第2题 整数算术运算总是会回环的
+
+print(math.maxinteger*2)
+print(math.mininteger*2)
+print(math.maxinteger*math.mininteger)
+print(math.mininteger*math.mininteger)
+--]]
+
+--[[ 第3题
+for i = -10,10 do
+print(i,i % 3)
+end
+--]]
+
+
+--[[ 第4题
+
+print(2^3^4)
+print(2^-3^4)
+--]]
+
